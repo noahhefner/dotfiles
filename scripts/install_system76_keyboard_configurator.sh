647c160b3b82b4c0133e198567858ab9441ace4a
Noah Hefner <nhhefner127+github@protonmail.com>
2023-09-17 13:55:03 -0400

    scripts

--- scripts/install_system76_keyboard_configurator.sh
+++ scripts/install_system76_keyboard_configurator.sh
@@ -1,14 +1,15 @@
 #/bin/bash
 
-username=$(id -u -n 1000)
-builddir=$(pwd)
+wd=$(pwd)
 dl_url="https://github.com/pop-os/keyboard-configurator/releases/download/v1.3.3/keyboard-configurator-1.3.2-x86_64.AppImage"
 appimage_name="keyboard-configurator-1.3.2-x86_64.AppImage"
 
 # System76 Launch Keyboard configurator
 echo "Installing System76 Launch Keyboard Configurator."
-cp ../assets/system76KCicon.svg "/home/$username/AppImages/System76KeyboardConfigurator"
-cp ../config/.desktop "/home/$username/.local/share/applications"
-cd "/home/$username/AppImages/System76KeyboardConfigurator"
+cp ../assets/system76KCicon.svg "/home/$USER/AppImages/System76KeyboardConfigurator"
+cp ../config/.desktop "/home/$USER/.local/share/applications"
+cd "/home/$USER/AppImages/System76KeyboardConfigurator"
 wget $dl_url
 chmod +x $appimage_name
+
+cd "$wd"
