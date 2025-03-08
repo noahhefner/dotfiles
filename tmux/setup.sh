e813f5cca7c8bbbe758a0c7631a772a970c7bda8
Noah Hefner <noahhefner@users.noreply.github.com>
2025-03-08 18:58:10 +0000

    Update setup.sh

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,4 +1,5 @@
 mkdir -p ~/.config/tmux/plugins
+cd ~/.config/tmux/plugins
 
-git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.config/tmux/plugins
-git clone https://github.com/tmux-plugins/tmux-sensible.git ~/.config/tmux/plugins
+git clone https://github.com/christoomey/vim-tmux-navigator.git
+git clone https://github.com/tmux-plugins/tmux-sensible.git
