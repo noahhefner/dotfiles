40982cf7f49edb17446d93a02d2075666c5fd58e
Noah Hefner <nhefner127+github@protonmail.com>
2025-08-12 06:19:25 -0400

    work

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo -e "\nInstalling desktop packages..."
 yay -S --noconfirm --needed \
     sway \
     swaybg \
@@ -20,11 +21,14 @@ yay -S --noconfirm --needed \
 DOTFILES_CONFIG=$HOME/.local/share/dotfiles/config
 
 # Copy greetd configuration files
+echo -e "\nCopying greetd configuration files..."
 sudo cp $DOTFILES_CONFIG/greetd/config.toml /etc/greetd/config.toml
 sudo cp $DOTFILES_CONFIG/greetd/sway-config /etc/greetd/sway-config
 
 # Enable greetd service
+echo -e "\nEnabling greetd service..."
 systemctl enable greetd.service
 
 # Change boot target to GUI mode
+echo -e "\nSetting default target to graphical..."
 sudo systemctl set-default graphical.target
