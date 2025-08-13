e3dafcea360b0969d07e0844cbf7e3bddad54cbf
Noah Hefner <nhefner127+github@protonmail.com>
2025-08-13 06:23:06 -0400

    work

--- install/app_config/config.sh
+++ install/app_config/config.sh
@@ -17,5 +17,3 @@ cp -R $DOTFILES_CONFIGS/kitty $HOME/.config/
 echo -e "\nCopying bash configuration files..."
 cp $DOTFILES_CONFIGS/bash/.bashrc $HOME/
 cp $DOTFILES_CONFIGS/bash/.bash_aliases $HOME/
-echo -e "\nSourcing .bashrc..."
-source $HOME/.bashrc
