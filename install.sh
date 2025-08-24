b8aba5820f889078faf2382d98edfbd37a45866a
Noah Hefner <nhefner127+github@protonmail.com>
2025-08-24 07:32:10 -0400

    work

--- install.sh
+++ install.sh
@@ -3,18 +3,6 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-clear
-echo -e "\nNoah's Linux Config\n"
-
-# Install git
-echo -e "\nInstalling git..."
-sudo pacman -Sy --noconfirm --needed git
-
-# Clone this repository
-echo -e "\nCloning dotfiles repo from: https://github.com/noahhefner/dotfiles.git"
-rm -rf $HOME/.local/share/dotfiles/
-git clone "https://github.com/noahhefner/dotfiles.git" $HOME/.local/share/dotfiles >/dev/null
-
 DOTFILES_INSTALL=$HOME/.local/share/dotfiles/install
 
 # Prerequisites
