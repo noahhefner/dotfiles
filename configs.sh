5058d6e468b48d14d202f07f8ec2526a005d27a5
Noah Hefner <nhefner127+github@protonmail.com>
2025-10-22 20:19:24 -0400

    mako notifications, fix waybar active workspace indicator

--- configs.sh
+++ configs.sh
@@ -3,11 +3,6 @@
 # wallpapers
 sudo cp -R ./wallpapers/* /usr/share/backgrounds/
 
-# greetd + regreet configs
-sudo cp ./config/greetd/config.toml /etc/greetd/config.toml
-sudo cp ./config/greetd/hyprland-config /etc/greetd/hyprland-config
-sudo cp ./config/greetd/regreet.toml /etc/greetd/regreet.toml
-
 # bash
 cp ./config/bash/.bashrc $HOME/
 cp ./config/bash/.bash_aliases $HOME/
@@ -15,6 +10,11 @@ cp ./config/bash/.bash_aliases $HOME/
 # fuzzel
 mkdir -p $HOME/.config/fuzzel/
 cp ./config/fuzzel/fuzzel.ini $HOME/.config/fuzzel/
+
+# greetd + regreet configs
+sudo cp ./config/greetd/config.toml /etc/greetd/config.toml
+sudo cp ./config/greetd/hyprland-config /etc/greetd/hyprland-config
+sudo cp ./config/greetd/regreet.toml /etc/greetd/regreet.toml
 
 # waybar
 mkdir -p $HOME/.config/waybar/
