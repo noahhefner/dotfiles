89e7d20d6e64db077dda0f361894a3f33df0cdb7
Noah Hefner <nhefner127+github@protonmail.com>
2025-10-25 09:08:21 -0400

    launch

--- configs.sh
+++ configs.sh
@@ -23,7 +23,7 @@ envsubst < ./config/waybar/style.css > $HOME/.config/waybar/style.css
 
 # hyprland
 mkdir -p $HOME/.config/hypr/
-cp ./config/hypr/hyprland.conf $HOME/.config/hypr/
+envsubst < ./config/hypr/hyprland.conf > $HOME/.config/hypr/hyprland.conf
 
 # waypaper
 mkdir -p $HOME/.config/waypaper/
