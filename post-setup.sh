9700847bf47fdc7c523d492dc1af84df48a49384
Noah Hefner <nhefner127+github@protonmail.com>
2025-11-18 20:13:34 -0500

    set librewolf as default web browser

--- post-setup.sh
+++ post-setup.sh
@@ -11,6 +11,9 @@ sudo cp ./web-apps/icons/* /usr/share/icons/dotfiles/
 # Change oh-my-bash theme to powerline
 sed -i "s/^OSH_THEME=\".*\"/OSH_THEME=\"powerline\"/" "$HOME/.bashrc"
 
+# Set Librewolf as default browser
+xdg-settings set default-web-browser librewolf.desktop
+
 # Enable avahi service
 sudo systemctl enable avahi-daemon
 
